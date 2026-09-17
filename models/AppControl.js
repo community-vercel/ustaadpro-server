536df87019975532b183485afe69a632632c8357
Anisul Islam <anis.inbox10@gmail.com>
2026-09-17 23:33:01 +0500

    fix: add allow_quantity to automatic db schema migrations to prevent 500 error on update

--- models/AppControl.js
+++ models/AppControl.js
@@ -190,6 +190,7 @@ class AppControl {
     await pool.query('ALTER TABLE categories ADD COLUMN IF NOT EXISTS sort_order INT NOT NULL DEFAULT 999');
     await pool.query('ALTER TABLE subcategories ADD COLUMN IF NOT EXISTS web_image_url TEXT');
     await pool.query('ALTER TABLE subcategories ADD COLUMN IF NOT EXISTS mobile_icon_url TEXT');
+    await pool.query('ALTER TABLE services ADD COLUMN IF NOT EXISTS allow_quantity BOOLEAN NOT NULL DEFAULT TRUE');
 
     await pool.query(`
       CREATE TABLE IF NOT EXISTS home_slides (
